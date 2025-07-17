42b95bbae5dad5a143327270ef3813abc9c3b7bc
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2025-07-17 11:20:50 +0530

    Add files via upload

---
+++ week11/7-6/patternprinting.js
@@ -0,0 +1,19 @@
+// let nums = 5;
+
+// for (let i = 1; i <= nums; i++) {
+//   let pattern = "*";
+//   for (let j = 1; j <= i; j++) {
+//     pattern += "*";
+//   }
+//   console.log(pattern);
+// }
+
+
+// if(n%2==0){
+//   console.log("prime");
+// }
+// else{
+//   console.log("not a prime");
+  
+// }
+
