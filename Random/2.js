82d5c804917b3efa6d25a326b1c60403ad8d47d8
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2025-07-27 19:48:50 +0530

    Add files via upload

---
+++ Random/2.js
@@ -0,0 +1,10 @@
+let factor="NA";
+let fplayer="Cristiano Rnaldo";
+let fmovie="Anbe Sivam";
+
+function favorite(){
+  console.log("Favorite Actor : "+factor);
+  console.log("Favorite Player : "+fplayer);
+  console.log("Favorite Movie : "+fmovie)
+}
+favorite()
