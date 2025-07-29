5b1b5d90f32c0aea89524e5b99c86cd439744e8e
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2025-07-29 08:58:21 +0530

    Add files via upload

---
+++ Random/5.js
@@ -0,0 +1,7 @@
+let fruit=['Apple','Banana','Orange','Pomegranate']
+console.log(fruit);
+console.log(fruit.length);
+
+for(i=0;i<fruit.length;i++){
+  console.log( "Array Elements : " ,fruit[i]);
+}
