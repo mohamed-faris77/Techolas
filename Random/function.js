4016f8d3e9890cfea97d17253653d8513c20b89c
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2025-08-07 11:05:56 +0530

    Add files via upload

---
+++ Random/function.js
@@ -0,0 +1,21 @@
+function nothing()
+{
+  console.log("Nothing Phone 2a")
+}
+
+function add()
+{
+  let a= 10;
+  let b= 20;
+  console.log(a+b)
+}
+
+function sub(){
+  let a=20
+  let b=10
+  console.log(a-b)
+}
+
+nothing()
+add()
+sub()
