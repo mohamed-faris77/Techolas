d5fdc0008f5ebee7e8f6d87c7f30e23e2edbc01f
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2025-08-08 10:57:55 +0530

    Add files via upload

---
+++ Random/ifelseex.js
@@ -0,0 +1,52 @@
+//console.log("Is it raining outside?")
+// let rain=true;
+// if (rain)
+// {
+//   console.log("It is raining outside");
+// }
+// else
+// {
+//   console.log("It is not raining outside");
+// }
+
+//console.log("Did you complete your homework?")
+// let homework=false;
+// if(homework)
+// {
+//   console.log("Good Job")
+// }
+// else
+// {
+//   console.log("Finish your home work soon!")
+// }
+
+// console.log("Are there any cookies left?")
+// var cookies= true;
+// if(cookies)
+// {
+//   console.log("Yes, would you like to have cookie")
+// }
+// else
+// {
+//   console.log("No, it's time to bake some cookies")
+// }
+
+// console.log("Did you brush your teeth?")
+// var teeth=false;
+// if(teeth)
+// {
+//   console.log("Your teeth are clean")
+// }
+// else
+// {
+//   console.log("Go brush your teeth now")
+// }
+
+console.log("Is it your birthday?")
+var birthday=false;
+if(birthday){
+  console.log("Happy birthday! Enjoy your special day!")
+}
+else{
+  console.log("Have a great day!")
+}
